6c899994066ee3e895a80388e5cc1af87c60cb04
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-21 14:43:25 -0700

    got login token after submitting correct username/password combination

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
-const Login = () => {
+const LoginForm = props => {
     const [ credentials, setCredentials ] = useState({
         username: '',
         password: ''
@@ -10,6 +11,23 @@ const Login = () => {
         setCredentials({
             ...credentials,
             [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        axiosWithAuth().post('/api/login', credentials)
+        .then(response => {
+            console.log(response)
+            window.localStorage.setItem('token', response.data.payload)
+            setCredentials({
+                username: '',
+                password: ''
+            })
+            props.history.push('/friends')
+        })
+        .catch(() => {
+            alert('This username and password combination is incorrect.')
         })
     }
 
@@ -26,11 +44,12 @@ const Login = () => {
                 />
                 <label htmlFor='password'>Password</label>
                 <input
+                    id='password'
                     name='password'
                     type='password'
                     onChange={handleChange}
                 />
-                <button>Log In</button>
+                <button onClick={handleSubmit}>Log In</button>
             </form>
         </div>
     )
