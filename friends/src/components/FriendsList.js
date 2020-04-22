c7b50394f3e93dd1f2426ef5a64d9f77c56574c3
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-21 20:00:39 -0700

    get and post data successfully mvp

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 import Friend from './Friend'
 
 const FriendsList = props => {
 
-    const getData = () => {
-        
+    const [ formValues, setFormValues ] = useState({
+        name: '',
+        age: '',
+        email: '',
+    })
+
+    const [ data, setData ] = useState([])
+
+    const [ isEditing, setIsEditing ] = useState(false)
+
+    // update state with new form values when form is updated
+    const updateForm = event => {
+        setFormValues({
+            ...formValues,
+            [event.target.name]: event.target.value
+        })
     }
 
-    const postData = () => {
-        
+    // submit get request to the server
+    const getData = event => {
+        event.preventDefault()
+        axiosWithAuth()
+            .get('/api/friends')
+            .then(response => {
+                // console.log(response)
+                setData(response.data)
+            })
+            .catch(error => alert(error))
+    }
+
+    // submit post request to the server
+    const postData = event => {
+        event.preventDefault()
+        axiosWithAuth()
+            .post('/api/friends', formValues)
+            .then(response => {
+                // console.log(response)
+                setData(response.data)
+            })
+            .catch(error => alert(error))
     }
 
     return(
         <>
         <form>
             <label htmlFor='name'> Name: 
-                <input name='name' id='name' value={props.name} onChange={updateForm}/>
+                <input name='name' id='name' value={formValues.name} onChange={updateForm}/>
             </label>
             <label htmlFor='age'> Age: 
-                <input name='age' id='age' value={props.age} onChange={updateForm}/>
+                <input name='age' id='age' value={formValues.age} onChange={updateForm}/>
             </label>
             <label htmlFor='email'> Email: 
-                <input name='email' id='email' value={props.email} onChange={updateForm}/>
+                <input name='email' id='email' value={formValues.email} onChange={updateForm}/>
             </label>
-            <button className='submitButtonGet' onClick={getData}>Submit Get</button>
-            <button className='submitButtonPost' onClick={postData}>Submit Post</button>
+            {isEditing ? <button>Submit Put</button> : 
+                <div>
+                    <button onClick={getData}>Submit Get</button>
+                    <button onClick={postData}>Submit Post</button>
+                </div>
+            }
+            
         </form>
         <div className="friends">
-            {props.data.map(friend => <Friend key={friend.id} friend={friend}/>)}
+            {data.map(friend => <Friend key={friend.id} friend={friend}/>)}
         </div>
         </>
     )
